0b82a6bab255ae7b550c7c92a20cb48e673518d8
Татьяна Ефремова <efita41@gmail.com>
2024-10-24 21:34:00 +0300

    add /details/..handler

--- app.rb
+++ app.rb
@@ -71,3 +71,11 @@ post '/new' do
  	redirect to '/'
  	
 end
+
+#вывод информации о посте
+
+get '/details/:post_id' do	
+	post_id = params[:post_id]
+
+	erb "Displaying information for post id #{post_id}"
+end
